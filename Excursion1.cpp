10e363a59b6909d7b827f8ac027029f3da9697e0
John Glass <50562358+johnglass2003@users.noreply.github.com>
2025-03-04 23:01:17 -0500

    basically all of it
    mm yea p much

--- Excursion1.cpp
+++ Excursion1.cpp
@@ -14,13 +14,12 @@ struct branchElement
 
     int sourceNode = 0;
     int destNode = 0;
-    int compVal = 0;
-
+    float compVal = 0.0f;
 };
 
 
 
-void readNetlist(vector<branchElement> &netlist)
+int readNetlist(vector<branchElement> &netlist)
 {
     fstream file;
     string line = "V";
@@ -31,6 +30,8 @@ void readNetlist(vector<branchElement> &netlist)
     int index = 0;
     int line_element = 0;
     branchElement sample;
+
+    int numRowsA = 0;
 
 
     file.open("netlist.txt", ios::in);
@@ -69,6 +70,7 @@ void readNetlist(vector<branchElement> &netlist)
             {
                 sourceN.push_back(line[line_element]);
                 line_element++;
+                numRowsA = max(numRowsA, stoi(sourceN));
             }
             line_element++;
             // STORES DEST NODE // 
@@ -76,10 +78,11 @@ void readNetlist(vector<branchElement> &netlist)
             {
                 destN.push_back(line[line_element]);
                 line_element++;
+                numRowsA = max(numRowsA, stoi(destN));
             }
             line_element++;
             // STORES COMP VAL // 
-            while (line[line_element] != '\n' )
+            while (line[line_element] != '\n' && line[line_element] != '\0')
             {
                 compVal.push_back(line[line_element]);
                 line_element++;
@@ -88,7 +91,8 @@ void readNetlist(vector<branchElement> &netlist)
             netlist[index].label = stoi(identifier);
             netlist[index].sourceNode = stoi(sourceN);
             netlist[index].destNode = stoi(destN);
-            netlist[index].compVal = stoi(compVal);
+            netlist[index].compVal = stof(compVal);
+
             index++;
             getline(file,line);
      
@@ -98,10 +102,24 @@ void readNetlist(vector<branchElement> &netlist)
         
 
         file.close();
-   
+        return numRowsA;
     }
     
+    void fillMatrix(vector<branchElement>& netlist, int matrixSize, int numNodes, vector<vector<float>>& matrix) {
+        // Initialize matrix with zeros
+        matrix = vector<vector<float>>(matrixSize, vector<float>(numNodes, 0.0f));
+    
+        int i = 0;
+        for (const auto& elem : netlist) {
+            int source = elem.sourceNode;
+            int dest = elem.destNode;
 
+            matrix[source][i] = 1;
+            matrix[dest][i] = -1;
+
+            i++;
+        }
+    }
 
 
 
@@ -109,9 +127,113 @@ void readNetlist(vector<branchElement> &netlist)
 int main()
 {
     vector<branchElement> netlist;
-    readNetlist(netlist);
-    string test = "1234";
-    int bruh = stoi(test);
+    int matrixSize = 0;
+
+    // Read netlist and fill a matrix
+    matrixSize = readNetlist(netlist) + 1;
+
+    int x = netlist.size();
+    vector<vector<float>> aMatrix(matrixSize, vector<float>(x, 0.0f));
+
+    fillMatrix(netlist, matrixSize, x, aMatrix);
+
+    // Remove the first row of the matrix
+    aMatrix.erase(aMatrix.begin());
+
+    int aRows = aMatrix.size();
+    int aCols = aMatrix[0].size();
+
+    // Transpose the matrix
+    vector<vector<float>> ATMatrixNeg(aCols, vector<float>(aRows, 0.0f));
+
+    int aTRows = ATMatrixNeg.size();
+    int aTCols = ATMatrixNeg[0].size();
+
+    for (int i = 0; i < aTRows; ++i) {
+        for (int j = 0; j < aTCols; ++j) {
+            ATMatrixNeg[i][j] = -1 * aMatrix[j][i];
+        }
+    }
+
+    vector<vector<float>> iMatrix(x, vector<float>(x, 0.0f));
+
+    for (int i = 0; i < x; ++i) {
+        iMatrix[i][i] = 1;
+    }
+
+    vector<vector<float>> fMatrix(x, vector<float>(x, 0.0f));
+
+    for (int i = 0; i < x; ++i) {
+        if (netlist[i].type == 'R') {
+            fMatrix[i][i] = -netlist[i].compVal;
+        }
+    }
+
+    vector<vector<float>> u(x, vector<float>(1, 0.0f));
+
+    for (int i = 0; i < x; ++i) {
+        if (netlist[i].type == 'V') {
+            u[i][0] = netlist[i].compVal;
+        }
+    }
+
+    int tSize = (matrixSize - 1) + 2 * x;
+    vector<vector<float>> T(tSize, vector<float>(tSize, 0.0f));
+
+    for (int i = 0; i < aRows; ++i) {
+        for (int j = 0; j < aCols; ++j) {
+            T[i][j + tSize - x] = aMatrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < aTRows; ++i) {
+        for (int j = 0; j < aTCols; ++j) {
+            T[i + aRows][j] = ATMatrixNeg[i][j];
+            if (T[i + aRows][j] == -0.0f) {
+                T[i + aRows][j] = 0.0f;
+            }
+        }
+    }
+
+    for (int i = 0; i < x; ++i) {
+        for (int j = 0; j < x; ++j) {
+            T[i + aRows][j + aTCols] = iMatrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < x; ++i) {
+        for (int j = 0; j < x; ++j) {
+            T[i + aRows + x][j + aTCols] = iMatrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < x; ++i) {
+        for (int j = 0; j < x; ++j) {
+            T[i + tSize - x][j + tSize - x] = fMatrix[i][j];
+        }
+    }
+
+    // Create a new matrix that is a copy of T but with one more column
+    vector<vector<float>> eMatrix(tSize, vector<float>(tSize + 1, 0.0f));
+
+    for (int i = 0; i < tSize; ++i) {
+        for (int j = 0; j < tSize; ++j) {
+            eMatrix[i][j] = T[i][j];
+        }
+    }
+
+    for (int i = 0; i < x; ++i) {
+        eMatrix[i + tSize - x][tSize] = u[i][0];
+    }
+
+    // Print the new matrix
+    cout << "" << endl;
+    for (int i = 0; i < tSize; ++i) {
+        for (int j = 0; j < tSize + 1; ++j) {
+            cout << eMatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
 
 
 
